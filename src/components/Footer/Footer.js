01bb01008ec98268612bf19977ecd4e8426b2172
isakbet <yosephbet@gmail.com>
2020-07-20 22:34:00 +0200

    Add icon

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import './Footer.css';
 
@@ -20,26 +19,3 @@ function Footer() {
 }
 
 export default Footer;
-=======
-import React from 'react';
-import './Footer.css';
-
-
-function Footer() {
-
-  return (
-    <div className="footer">
-      <footer className="p-1 bg-dark">
-        <div className="mb-2">
-          <a className="badge badge-light" href="https://github.com/isakbet" target="_blank" rel="noopener noreferrer">GitHub</a>
-        </div>
-        <div>
-          <span className="mr-3 text-warning">Built by <a href="https://isakbet.github.io/Personal-Portfolio/" target="_blank" rel="noopener noreferrer">Yoseph Berhane</a></span>
-        </div>
-      </footer>
-    </div>
-  )
-}
-
-export default Footer;
->>>>>>> master
