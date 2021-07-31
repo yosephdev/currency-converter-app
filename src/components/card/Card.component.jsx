75b7d3610f13deae74d8ca661a5b6c894e369ae5
isakbet <yosephbet@gmail.com>
2021-07-31 19:27:13 +0200

    Update flag

--- src/components/card/Card.component.jsx
+++ src/components/card/Card.component.jsx
@@ -3,7 +3,7 @@ import './card.styles.css';
 
 const Card = (props) => (
     <div className="card-container">
-        <img className="card-img" alt="country flag" src={`https://www.countryflags.io/${props.currency.name.substring(0,2)}/shiny/64.png`}/>
+        <img className="card-img" alt="country flag" src={`https://www.countryflags.io/${props.currency.name.substring(0,2)}/flat/64.png`}/>
         <h2>{props.currency.name}</h2>
         <p>€1 EUR =  {props.currency.rate.toFixed(2)} {props.currency.name}</p>
     </div>
