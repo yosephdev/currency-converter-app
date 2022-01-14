41fd8f77c2e171faebad0464979bee4c35de71d6
Yoseph Berhane <yosephbet@gmail.com>
2022-01-14 14:17:00 +0100

    Change navbar and link

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -8,12 +8,12 @@ function TopBar({ location }) {
 
   return (
     <Navbar bg="navbar navbar-dark">
-      <Navbar.Brand href="#home">Currency Converter V2</Navbar.Brand>
+      <Navbar.Brand href="#home">Currency Converter</Navbar.Brand>
       <Navbar.Toggle aria-controls="navbarTogglerDemo03" />
       <Navbar.Collapse id="navbarTogglerDemo03">
         <Nav className="mr-auto mt-2 mt-lg-0">
           <Nav.Link href="/" active={pathname === "/"}>
-            Table
+            Home
           </Nav.Link>          
           <Nav.Link
             href="/rates"
