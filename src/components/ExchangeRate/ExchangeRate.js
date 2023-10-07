12a1a10d8c79513f82b85d900f8e738696ffc500
Yoseph Berhane <yosephbet@gmail.com>
2023-10-07 10:07:23 +0200

    Create Functional Component

--- src/components/ExchangeRate/ExchangeRate.js
+++ src/components/ExchangeRate/ExchangeRate.js
@@ -1,1 +1,18 @@
 import React, { useEffect, useState } from "react";
+
+const ExchangeRate = () => {
+  const [exchangeRates, setExchangeRates] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Fetch exchange rates here and update state
+    // Set loading to false once data is fetched
+  }, []); // Empty dependency array means this useEffect runs once on mount
+
+  return (
+    <div>
+      {/* Render exchange rates or loading indicator based on loading state */}
+    </div>
+  );
+};
+
